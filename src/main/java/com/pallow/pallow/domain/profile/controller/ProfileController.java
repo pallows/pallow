d786995fd4ff609a33c986790d320f9bf2ec1aa2
fenec_fox <atimevil@gmail.com>
2024-07-21 20:18:15 +0900

    refactor : import 정리

--- src/main/java/com/pallow/pallow/domain/profile/controller/ProfileController.java
+++ src/main/java/com/pallow/pallow/domain/profile/controller/ProfileController.java
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
