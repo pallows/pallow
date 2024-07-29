0e3f6770277acc14045b1c1ca0aee4be820998f9
USER <jioung_1020@naver.com>
2024-07-29 19:57:27 +0900

    fix : Review entity, controller, dto에 starRating<별점> 추가

--- src/main/java/com/pallow/pallow/domain/meetsreview/controller/ReviewController.java
+++ src/main/java/com/pallow/pallow/domain/meetsreview/controller/ReviewController.java
@@ -8,14 +8,7 @@ import com.pallow.pallow.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,19 +18,20 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     /**
+     * 유지영 수정
      * 모임 리뷰 생성
      * @param meets_id  그룹 ID
-     * @param requestDto  생성 데이터 [content]
+     * @param requestDto  생성 데이터 [content, starRating]
      * @param userDetails 유저 데이터
      * @return 생성 성공 메시지 + 생성된 리뷰 데이터
      */
     @PostMapping()
-    public ResponseEntity<CommonResponseDto> createReview(@PathVariable Long meets_id,
+    public ResponseEntity<CommonResponseDto> createReview(
+            @PathVariable Long meets_id,
             @RequestBody ReviewRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return ResponseEntity.ok(
-                new CommonResponseDto(
-                        Message.REVIEW_CREATE_SUCCESS, reviewService.create(meets_id, requestDto, userDetails.getUser())));
+        reviewService.create(meets_id, requestDto, userDetails.getUser());
+        return ResponseEntity.ok(new CommonResponseDto(Message.REVIEW_CREATE_SUCCESS));
     }
 
     /**
