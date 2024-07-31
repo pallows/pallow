078415ed39e25beb124ff02be2e2d486638ffa24
fenec_fox <atimevil@gmail.com>
2024-07-31 11:29:10 +0900

    fix : 지역 삭제

--- src/main/java/com/pallow/pallow/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/pallow/pallow/domain/user/dto/UserResponseDto.java
@@ -1,20 +1,20 @@
-package com.pallow.pallow.domain.user.Dto;
+package com.pallow.pallow.domain.user.dto;
 
 import com.pallow.pallow.domain.user.entity.User;
+import com.pallow.pallow.global.enums.Region;
 import lombok.Getter;
 
 @Getter
 public class UserResponseDto {
+
     private final String username;
     private final String email;
     private final String nickname;
-    private final String position;
-    // private String Position
 
     public UserResponseDto(User user) {
         this.username = user.getUsername();
         this.email = user.getEmail();
         this.nickname = user.getNickname();
-        this.position = user.getPosition();
+
     }
 }
