2e9ff64704aa2387bebc2af1510422e4c22bf937
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-31 16:00:14 +0900

    fix : @restcontroller url에 "/api"추가하여 commoncontroller의 get매핑과 구분짓게 함

--- src/main/java/com/pallow/pallow/domain/invitedboard/controller/InvitedBoardController.java
+++ src/main/java/com/pallow/pallow/domain/invitedboard/controller/InvitedBoardController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/groups/{groupId}/invitation")
+@RequestMapping("/api/groups/{groupId}/invitation")
 @RequiredArgsConstructor
 public class InvitedBoardController {
 
