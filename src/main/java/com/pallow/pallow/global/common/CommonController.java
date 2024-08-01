26d1624a008a8fda1060941bfc38f8033470eaa8
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-08-02 04:29:31 +0900

    add : 당장 쓸 html 뷰 연동 및 url 추가

--- src/main/java/com/pallow/pallow/global/common/CommonController.java
+++ src/main/java/com/pallow/pallow/global/common/CommonController.java
@@ -1,10 +1,17 @@
 package com.pallow.pallow.global.common;
 
+import com.pallow.pallow.domain.meets.service.MeetsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @Controller
 public class CommonController {
+
+    private final MeetsService meetsService;
 
     @GetMapping("/")
     public String home() {return "login";}
@@ -24,9 +31,24 @@ public class CommonController {
         return "chat";
     }
 
+    @GetMapping("/benefits")
+    public String benefitsPage()  {
+        return "benefits";
+    }
+
+    @GetMapping("/payments")
+    public String paymentsPage()  {
+        return "payments";
+    }
+
     @GetMapping("/register_information")
     public String registerInformationPage() {
         return "register_information";
     }
 
+    @GetMapping("/meetsCollection")
+    public String meetsCollectionPage(Model model) {
+        model.addAttribute("meets", meetsService.getAllMeets());
+        return "meetsCollection";
+    }
 }
