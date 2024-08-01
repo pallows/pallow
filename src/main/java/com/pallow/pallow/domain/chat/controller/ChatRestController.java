b58b2e8108bac768ec754f5e488df3df68cdcb39
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-08-01 18:44:33 +0900

    add : 채팅 메시지 시간 기능 추가 modified : 채팅 메시지 username -> nickname받게 설정 add : 채팅 reaction 기능(공감) 추가

---
+++ src/main/java/com/pallow/pallow/domain/chat/controller/ChatRestController.java
@@ -0,0 +1,8 @@
+package com.pallow.pallow.domain.chat.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatRestController {
+
+}
--- src/main/java/com/pallow/pallow/domain/chat/dto/ChatMessageDto.java
+++ src/main/java/com/pallow/pallow/domain/chat/dto/ChatMessageDto.java
@@ -21,6 +21,12 @@ public class ChatMessageDto {
         private String content;
         private MessageType type;
         private LocalDateTime createdAt;
-        private LocalDateTime modifiedAt;
+        private String formattedTime;
+        private int ChatReactionCount;
+
+        /**
+         * 현재 사용자의 반응 여부
+         */
+        private boolean hasChatReacted;
 
 }
