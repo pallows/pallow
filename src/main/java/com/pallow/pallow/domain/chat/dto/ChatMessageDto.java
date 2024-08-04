97e141196543b47fc68b04c50572a6e4fcc955e9
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-08-05 02:49:46 +0900

    add : ChatMessageDto에 @builder추가

--- src/main/java/com/pallow/pallow/domain/chat/dto/ChatMessageDto.java
+++ src/main/java/com/pallow/pallow/domain/chat/dto/ChatMessageDto.java
@@ -22,11 +22,22 @@ public class ChatMessageDto {
         private MessageType type;
         private LocalDateTime createdAt;
         private String formattedTime;
-        private int ChatReactionCount;
+        private int chatReactionCount;
 
         /**
          * 현재 사용자의 반응 여부
          */
         private boolean hasChatReacted;
 
+        ChatMessageDto message = ChatMessageDto.builder()
+                .id(1L)
+                .chatRoomId(2L)
+                .sender("User")
+                .content("Hello")
+                .type(MessageType.CHAT)
+                .createdAt(LocalDateTime.now())
+                .formattedTime("10:30 AM")
+                .chatReactionCount(5)
+                .hasChatReacted(true)
+                .build();
 }
