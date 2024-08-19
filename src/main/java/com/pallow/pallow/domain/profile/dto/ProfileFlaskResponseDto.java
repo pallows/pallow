e8bcf74afff5859effe4531832772f4918d8553f
dohoonkim <mianhe89@gmail.com>
2024-08-19 17:55:38 +0900

    feat : 메인페이지 nick 프로필 조회 기능 수정

--- src/main/java/com/pallow/pallow/domain/profile/dto/ProfileFlaskResponseDto.java
+++ src/main/java/com/pallow/pallow/domain/profile/dto/ProfileFlaskResponseDto.java
@@ -1,6 +1,7 @@
 package com.pallow.pallow.domain.profile.dto;
 
 import com.pallow.pallow.domain.profile.enums.Mbti;
+import com.pallow.pallow.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +13,6 @@ public class ProfileFlaskResponseDto {
     private String name;
     private Mbti mbti;
     private String image;
+    private String nickName;
+    // 여기에 닉네임이 들어가야
 }
